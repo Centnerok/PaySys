f9b05ac8833b30d1f6bb3a63f3b09bf961d05f24
Andrew <centner36@gmail.com>
2026-09-12 14:46:11 +0300

    spring project initialization

---
+++ src/main/java/centnerok/paysys/PaysysApplication.java
@@ -0,0 +1,13 @@
+package centnerok.paysys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaysysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/centnerok/paysys/PaysysApplicationTests.java
@@ -0,0 +1,13 @@
+package centnerok.paysys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
