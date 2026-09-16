80ef951f129aa4faa4889cddf7923f4940abfb4b
Andrew <centner36@gmail.com>
2026-09-16 12:39:50 +0300

    created account controller

---
+++ src/main/java/centnerok/paysys/controller/AccountController.java
@@ -0,0 +1,59 @@
+package centnerok.paysys.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import centnerok.paysys.model.dto.BalanceResponse;
+import centnerok.paysys.model.dto.DepositRequest;
+import centnerok.paysys.model.dto.TransactionResponse;
+import centnerok.paysys.model.dto.TransferRequest;
+import centnerok.paysys.service.AccountService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+    private final AccountService service;
+
+    public AccountController(AccountService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<TransactionResponse> transfer(@Valid @RequestBody TransferRequest request) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+                .body(service.makeTransfer(request));
+    }
+    
+    @PostMapping("/{accountId}deposit")
+    public ResponseEntity<TransactionResponse> deposit(@PathVariable Long accountId, @Valid @RequestBody DepositRequest request) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+                .body(service.makeDeposit(accountId, request));
+    }
+
+    @GetMapping("/{accountId}/balance")
+    public ResponseEntity<BalanceResponse> getBalance(@PathVariable Long accountId) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+                .body(service.getBalance(accountId));
+    }
+
+    @GetMapping("/{accountId}/transactions")
+    public ResponseEntity<List<TransactionResponse>> getTransactions(@PathVariable Long accountId) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+                .body(service.getTransactions(accountId));
+    }
+    
+    
+}
